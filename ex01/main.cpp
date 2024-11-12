b176c244521f6fe35ce97e46b5ac236671146a29
Fefeco <carranzafederico@gmail.com>
2024-11-12 13:15:47 +0100

    ex01 - Edit makefile's target name. Create main.cpp as the subject ask

---
+++ ex01/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 13:09:43 by fcarranz          #+#    #+#             */
+/*   Updated: 2024/11/12 13:11:16 by fcarranz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.h"
+
+#include <iostream>
+
+int main(void) {
+
+  Fixed a;
+  Fixed const b(10);
+  Fixed const c(42.42f);
+  Fixed const d(b);
+
+  a = Fixed(1234.4321f);
+
+  std::cout << "a is " << a << std::endl;
+  std::cout << "b is " << b << std::endl;
+  std::cout << "c is " << c << std::endl;
+  std::cout << "d is " << d << std::endl;
+
+  std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+  std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+  std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+  std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+  return 0;
+}
