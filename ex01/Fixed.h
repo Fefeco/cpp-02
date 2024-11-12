2c9edcca0abb334335030037ab496050678b97e4
Fefeco <carranzafederico@gmail.com>
2024-11-12 13:21:25 +0100

    ex01 - Implement Fixed.h

---
+++ ex01/Fixed.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 13:07:56 by fcarranz          #+#    #+#             */
+/*   Updated: 2024/11/12 13:20:28 by fcarranz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+class Fixed {
+
+public:
+  Fixed(void);
+  ~Fixed(void);
+
+  Fixed(const Fixed &other);
+  Fixed &operator=(const Fixed &other);
+
+  Fixed(const int nb);
+  Fixed(const float nb);
+
+  int getRawBits(void) const;
+  void setRawBits(const int raw);
+  float toFloat(void) const;
+  int toInt(void) const;
+
+private:
+  static const int width = 8;
+  int fixedPoint;
+};
+
+std::ostream &operator<<(const std::ostream &stream, const Fixed &fixed);
